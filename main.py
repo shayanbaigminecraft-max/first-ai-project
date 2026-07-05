c1d8d5b34902b7e4d49916846d6af3a20e3f6342
shayanbaigminecraft-max <shayanbaigminecraft@gmail.com>
2026-07-06 00:53:36 +0500

    local package created

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+from setuptools import find_packages, setup
+
+setup(
+    name="mcq-generator",
+    version="0.0.1",
+    author="Shayan Baig",
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "langchain-core",
+        "langchain-mistralai",
+        "streamlit",
+        "python-dotenv",
+        "pypdf2"
+    ]
+)
